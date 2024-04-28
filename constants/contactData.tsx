a9ae784db7f3127bbac3d3a4ba8c03e8e0610703
Saroj Adhikari <sarojadhikari076@gmail.com>
2024-04-28 22:08:34 +0100

    Update address in contactData.tsx

--- constants/contactData.tsx
+++ constants/contactData.tsx
@@ -3,7 +3,7 @@ import { Email, LocationOn, Phone } from '@mui/icons-material'
 export const contactData = [
   {
     label: 'Address',
-    value: '11 Devonshire House, Loughborough, LE11 3DW',
+    value: '11 Devonshire House, Loughborough, LE113DW',
     icon: <LocationOn />,
     link: 'https://google.com/maps?q=11+Devonshire+House,+Loughborough,+LE11+3DW'
   },
